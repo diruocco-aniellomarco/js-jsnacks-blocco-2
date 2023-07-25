61dd6907b97dca6a7a303bc53993b7edec4c8d1e
diruocco-aniellomarco <aniello.marco.dr@gmail.com>
2023-07-25 17:38:57 +0200

    Feat: JSnack3

---
+++ JSnack3/js/main.js
@@ -0,0 +1,40 @@
+const listRandom = document.getElementById('list_random');
+const listOdd = document.getElementById('list_odd');
+const sumOddEl = document.getElementById('sum_odd_el');
+
+let listNumber = [];
+let sumOdd = 0;
+
+for (let i = 0; i < 10; i++) {
+    
+    let numRandom = Math.floor(Math.random() * 10) + 1;
+
+    listNumber[i] = numRandom;
+
+    if ( i != 9) {
+        listRandom.innerHTML += listNumber[i] + ', ';
+    }
+    // Per non far venire la virgola nella parte finale della lista
+    else {
+        listRandom.innerHTML += listNumber[i];
+    }
+    
+    if ( i % 2 != 0) {
+        if ( i != 9) {
+            listOdd.innerHTML += listNumber[i] + ', ';
+        }
+        // Per non far venire la virgola nella parte finale della lista
+        else {
+            listOdd.innerHTML += listNumber[i];
+        }
+
+        sumOdd = sumOdd + listNumber[i];
+        
+        console.log(listNumber[i]);
+        console.log(i);
+    };
+};
+
+console.log(listNumber);
+console.log('somma: ' + sumOdd);
+sumOddEl.innerHTML = sumOdd;
---
+++ JSnack3/readme.md
@@ -0,0 +1,9 @@
+# JSnack3
+
+## Traccia
+Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+
+## Svolgimento
+- Creo 1 arrey che conterrà dei numeri interi
+- una variante che servirà per immagazzinare la somma di tutti i numeri in posizione dispari.
+- mostro la somma dei numeri in posizione dispari
